e44f115fbe1f7d1a6a35028f991166747003e364
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-08-26 11:13:09 +0000

    Changes to be committed: 	modified:   app/cards/cards.css 	modified:   app/user/page.tsx 	modified:   components/card1/card.css

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -7,11 +7,11 @@ export default async function user() {
       const res = await data.json();
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <main className="flex min-h-screen flex-col items-center justify-center pt-24 px-5">
             <h1 className='text-5xl'>Liste des chauffeurs</h1>
-            <div className="grid lg:grid-cols-5 mt-5 gap-3 md:grid-cols-1">
+            <div className="grid lg:grid-cols-5 mt-5 gap-3 md:grid-cols-1 mt-5">
                 {res.map((user:any) => (
-                    <div key={user.id} className="border-solid border-blue-950 rounded-md border-2 p-2 md:w-full">
+                    <div key={user.id} className="border-solid border-blue-950 rounded-md border-2 p-2 w-full">
                         <h2>{user.id}</h2>
                         <h2 className="uppercase ">{user.full_name}</h2>
                     </div>
